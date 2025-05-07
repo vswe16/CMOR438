d180d01b1e66b8e384b74ace35a92b1f2b237eb9
Valerie Swe <143146645+vswe16@users.noreply.github.com>
2025-05-06 22:54:22 -0500

    Update test_decision_tree_pricer.py

--- Custom Machine Learning Package/tests/test_decision_tree_pricer.py
+++ Custom Machine Learning Package/tests/test_decision_tree_pricer.py
@@ -2,7 +2,6 @@ import numpy as np
 from airbnb_pricing_package.decision_tree_pricer import DecisionTreePricer
 
 def test_decision_tree_predict():
-    # Simple synthetic test data
     X = np.array([[1], [2], [3], [4]])
     y = np.array(["low", "low", "high", "high"])
 
